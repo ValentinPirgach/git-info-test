3a336e2e4442836bd69cf9ca8b2f6f03162f180b
Valentyn Pirhach <valentin.pirgach@gmail.com>
2018-11-01 22:18:15 +0200

    Make component as a function

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,55 +1,44 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { Card, List, Icon } from 'antd'
 import { RepoItem } from 'components'
 
-export default class Repos extends Component {
-  static propTypes = {
-    user: PropTypes.shape({
-      empty: PropTypes.bool,
-    }).isRequired,
-    repos: PropTypes.shape({
-      isFetching: PropTypes.bool,
-      list: PropTypes.arrayOf(PropTypes.shape({
-        name: PropTypes.string
-      }))
-    }).isRequired
+const Repos = (props) => {
+  const { user, repos: { list } } = props
+
+  if(props.repos.isFetching) {
+    return <Icon type="loading" style={{ fontSize: '36px' }} theme="outlined" />
   }
 
-  constructor(props) {
-    super(props);
+  if(user.empty) return null
 
-    this.state = {
-
-    };
-  }
-
-  render() {
-    const { user, repos: { list } } = this.props
-
-    console.log(this.props.repos);
-    if(this.props.repos.isFetching) {
-      return <Icon type="loading" style={{ fontSize: '36px' }} theme="outlined" />
-    }
-
-    if(user.empty) return null
-
-    return (
-      <Wrapper>
-        <Card title='Repositories'>
-          <List
-            itemLayout="horizontal"
-            dataSource={list}
-            renderItem={item => <RepoItem item={item}/>}
-          />
-        </Card>
-      </Wrapper>
-    );
-  }
+  return (
+    <Wrapper>
+      <Card title='Repositories'>
+        <List
+          itemLayout="horizontal"
+          dataSource={list}
+          renderItem={item => <RepoItem item={item}/>}
+        />
+      </Card>
+    </Wrapper>
+  );
 }
 
+Repos.propTypes = {
+  user: PropTypes.shape({
+    empty: PropTypes.bool,
+  }).isRequired,
+  repos: PropTypes.shape({
+    isFetching: PropTypes.bool,
+    list: PropTypes.arrayOf(PropTypes.shape({
+      name: PropTypes.string
+    }))
+  }).isRequired
+}
 
+export default Repos
 
 const Wrapper = styled.div`
   text-align: left;
